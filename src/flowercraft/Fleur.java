3b8b2f20eea461523894a340c1380bd56140e6ba
hmed2z <74240182+hmed2z@users.noreply.github.com>
2022-01-31 16:44:27 +0100

    Update Fleur.java

--- src/flowercraft/Fleur.java
+++ src/flowercraft/Fleur.java
@@ -2,8 +2,33 @@ package flowercraft;
 
 public class Fleur {
 
-    public Fleur()
-    {
+    public enum TypeFleur {1,2,3}
+    public TypeFleur type ;
+    public int abcisseFleur ;
+    public int ordonneeFleur ;
+    public int vitessePousse ;
 
+    public Fleur (TypeFleur type, int X, int Y) {
+        this.type = type ;
+        this.abcisseFleur = X ;
+        this.ordonneeFleur = Y ;
+        //this.vitessePousse = type*2 ;
     }
+
+    //int getVitesse (TypeFleur type) { return type*2 ; }
+
+    int getX () {
+        return abcisseFleur ;
+    }
+
+    int getY () {
+        return ordonneeFleur ;
+    }
+
+    TypeFleur getType () {
+        return type ;
+    }
+
+    //boolean isMature (Fleur fleur) {}
+
 }
