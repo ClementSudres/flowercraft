693d9020415708fb9dc440589cf84c641a7bc1b6
hmed2z <74240182+hmed2z@users.noreply.github.com>
2022-01-31 16:45:40 +0100

    Update Batiment.java

--- src/flowercraft/Batiment.java
+++ src/flowercraft/Batiment.java
@@ -2,8 +2,27 @@ package flowercraft;
 
 public class Batiment {
 
-    public Batiment()
-    {
+    public enum TypeBat {PRINCIPAL , PRODUCTION , DEFENSE}
+    public TypeBat type ;
+    public int abcisseBatiment ;
+    public int ordonneeBatiment ;
 
+    public Batiment (TypeBat type, int X , int Y) {
+        this.type = type ;
+        this.abcisseBatiment = X ;
+        this.ordonneeBatiment = Y ;
     }
+
+    int getX () {
+        return abcisseBatiment ;
+    }
+
+    int getY () {
+        return ordonneeBatiment ;
+    }
+
+    TypeBat getType () {
+        return type ;
+    }
+
 }
