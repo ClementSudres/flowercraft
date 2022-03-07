4d254eac168de1485729a1dffbe34e8869498689
hmed2z <74240182+hmed2z@users.noreply.github.com>
2022-03-07 14:37:15 +0100

    Update Fleur.java

--- src/flowercraft/Fleur.java
+++ src/flowercraft/Fleur.java
@@ -2,23 +2,53 @@ package flowercraft;
 
 public class Fleur {
 
-    public enum TypeFleur {LENT, MOYEN, RAPIDE}
+    public enum TypeFleur {LENT , MOYEN , RAPIDE}
     public TypeFleur type ;
-    public int positionFleur;
+    public int positionFleur ;
     public int vitessePousse ;
+    public boolean maturiteFleur ;
+    public int prix ;
 
     public Fleur (TypeFleur type, int idCase) {
         this.type = type ;
-        this.positionFleur = idCase;
-        //this.vitessePousse;
+        this.positionFleur = idCase ;
+        this.maturiteFleur = false ;
+        if (this.type == TypeFleur.LENT) {
+            this.vitessePousse = 30;
+            this.prix = 30 ;
+        }
+        else if (this.type == TypeFleur.MOYEN) {
+            this.vitessePousse = 20;
+            this.prix = 20 ;
+        }
+        else {
+            this.vitessePousse = 10 ;
+            this.prix = 10 ;
+        }
     }
 
-    //int getVitesse (TypeFleur type) { return type*2 ; }
-
-    TypeFleur getType () {
-        return type ;
+    int getPositionFleur (Fleur fleur) {
+        return fleur.positionFleur ;
     }
 
-    //boolean isMature (Fleur fleur) {}
+    TypeFleur getType (Fleur fleur) {
+        return fleur.type ;
+    }
+
+    void setMaturite (Fleur fleur) {
+        fleur.maturiteFleur = true ;
+    }
+
+    boolean getMaturite (Fleur fleur) {
+        return fleur.maturiteFleur ;
+    }
+
+    boolean estMature (Fleur fleur) {
+        return fleur.maturiteFleur ;
+    }
+
+    int getVitessePousse (Fleur fleur) {
+        return fleur.vitessePousse ;
+    }
 
 }
