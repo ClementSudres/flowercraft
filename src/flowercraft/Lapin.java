c85e7b84308d3f4404bf0988b48ba1deefa52ea8
hmed2z <74240182+hmed2z@users.noreply.github.com>
2022-03-21 14:41:27 +0100

    Update Lapin.java

--- src/flowercraft/Lapin.java
+++ src/flowercraft/Lapin.java
@@ -1,9 +1,28 @@
 package flowercraft;
 
+import java.util.Random ;
+
 public class Lapin {
 
-    public Lapin(int X,int Y)
-    {
+    public int positionLapin ;
+    public boolean estRassasie ;
+    public boolean estTropEffraye ;
 
+
+    public Lapin (int idCase) {
+        this.positionLapin = idCase ;
+        this.estRassasie = false ;
+        this.estTropEffraye = false ;
     }
+
+    public int getPositionLapin() {
+        return positionLapin;
+    }
+    /*
+    void apparitionLapin () {
+        Random r = new Random() ;
+        for (int i = 0 ; i <)
+    }
+    */
+
 }
